1f61685b89dcc07e19798dbe25d4500d1a722224
xiayulu <41274826+xiayulu@users.noreply.github.com>
2018-11-25 10:38:21 +0800

    Update CentOsPython.sh

--- CentOsPython.sh
+++ CentOsPython.sh
@@ -8,6 +8,12 @@ git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Crese
 yum install zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+## Install neovim
+yum remove vim
+yum install neovim
+pip3 install neovim
+curl -sLf https://spacevim.org/install.sh | bash
+
 ## Install python3
 # Before install python3.
 yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gcc make
@@ -25,11 +31,4 @@ make && make install
 ln -s /usr/local/python3/bin/python3 /usr/bin/python3
 ln -s /usr/local/python3/bin/pip3 /usr/bin/pip3
 
-curl https://raw.githubusercontent.com/kennethreitz/pipenv/master/get-pipenv.py | python
-
-## Install neovim
-yum remove vim
-yum install neovim
-pip3 install neovim
-curl -sLf https://spacevim.org/install.sh | bash
-nvim
+pip3 install --user --upgrade pipenv
